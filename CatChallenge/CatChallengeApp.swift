7a9d4b6245d277067a9b964ac4e43502bce5f8bd
goncaloalmeida <goncaloalmeida>
2024-06-03 10:03:02 +0100

    Initial Commit

---
+++ CatChallenge/CatChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatChallengeApp.swift
+//  CatChallenge
+//
+//  Created by goncaloalmeida on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
